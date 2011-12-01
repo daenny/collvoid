8888a4d14f7af2276a5cd34b44ca3e939a538d25
danielclaes <devnull@localhost>
2011-11-30 17:22:23 -0800

    cleanup and config

--- collvoid_controller/nodes/controllerRobots.py
+++ collvoid_controller/nodes/controllerRobots.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('collvoid_controller')
 import rospy
-import commands
 import string
-import sys
-import math
 import actionlib
 from collvoid_local_planner.srv import InitGuess
 from std_msgs.msg import String
@@ -14,13 +11,6 @@ from collvoid_msgs.msg import PoseTwistWithCovariance
 from move_base_msgs.msg import *
 
 import tf
-
-THRESHOLD = 0.1
-
-
-def dist(a, b):
-    return math.sqrt(math.pow(a.position.x - b.position.x, 2) + math.pow(a.position.y - b.position.y, 2))
-
 
 class ControllerRobots():
 
@@ -34,54 +24,26 @@ class ControllerRobots():
         self.client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
         self.client.wait_for_server()
        
-    
-        #self.pub_goal = rospy.Publisher("move_base/goal", MoveBaseActionGoal)
-     
         self.init_guess_srv = rospy.ServiceProxy("init_guess_pub", InitGuess)
-
-#        self.pub_commands_robot = rospy.Publisher("/commands_robot", String)
-#        self.sub_common_position = rospy.Subscriber("/position_share", PoseTwistWithCovariance, self.cb_common_positions)
         self.sub_commands_robot = rospy.Subscriber("/commands_robot", String, self.cb_commands_robot)
 
         self.hostname = rospy.get_namespace()
+
         if (self.hostname == "/"):
             self.hostname = gethostname()
             self.goals = rospy.get_param("/%s/goals"%self.hostname)
         else:
             self.goals = rospy.get_param("%sgoals"%self.hostname)
+
         rospy.loginfo("goals: %s"%str(self.goals))
         self.cur_goal = 0
         self.num_goals = len(self.goals["x"])
-#        rospy.loginfo("num Goals %d"%self.num_goals)
-        rospy.loginfo("Cur Goal %s"%str(self.return_cur_goal()))
         self.cur_goal_msg = self.return_cur_goal()
         self.circle = False
         rospy.loginfo("Name: %s",self.hostname)
 
-
-
-    def cb_common_positions(self,msg):
-        if self.stopped:
-            return
-        #       rospy.loginfo("%s"%rospy.get_master())
-        if msg.id == self.hostname:
-           
-            if dist(msg.pose.pose, self.cur_goal_msg.target_pose.pose) < THRESHOLD:
-                rospy.loginfo("Reached goal, sending new goal")
-                self.cur_goal += 1
-                if (self.cur_goal == self.num_goals):
-                    self.cur_goal = 0
-                self.cur_goal_msg = self.return_cur_goal()
-                if self.circle:
-                    str = "%s Start"%self.hostname
-                    self.pub_commands_robot.publish(String(str))
-            return
-        else:
-            return
-    
     def return_cur_goal(self):
         goal = MoveBaseGoal()
-        #rospy.loginfo("goal: %s"%str(goal))
         
         goal.target_pose.pose.position.x = self.goals["x"][self.cur_goal]
         goal.target_pose.pose.position.y = self.goals["y"][self.cur_goal]
@@ -91,7 +53,6 @@ class ControllerRobots():
         goal.target_pose.pose.orientation.z = q[2]
         goal.target_pose.pose.orientation.w = q[3]
         
-#        goal.target_pose.header.stamp = rospy.get_time()
         goal.target_pose.header.frame_id = "/map"
         return goal
 
@@ -100,23 +61,12 @@ class ControllerRobots():
 
 
     def cb_commands_robot(self,msg):
-        if msg.data == "all WP On" or msg.data == "%s WP On"%self.hostname:
-            rospy.loginfo("I am ON")
-            self.stopped = False
-
-        if msg.data == "all WP Off" or msg.data == "%s WP Off"%self.hostname:
-            self.stopped = True
-            rospy.loginfo("I am Off")
-
         if (msg.data == "all init Guess"):
             self.init_guess_srv(0.01)
         
         if msg.data == "all Stop" or msg.data == "%s Stop"%self.hostname:
             self.client.cancel_all_goals()
           
-        if self.stopped:
-            return
-
         if msg.data == "all Start" or msg.data == "%s Start"%self.hostname:
             self.client.send_goal(self.cur_goal_msg)
         
@@ -129,26 +79,10 @@ class ControllerRobots():
 
             rospy.loginfo("Send new Goal")            
 
-        if msg.data == "all Circle On" or msg.data == "%s Circle On"%self.hostname:
-            rospy.loginfo("I am on Circlemode")
-            self.circle = True
-            
-        if msg.data == "all Circle Off" or msg.data == "%s Circle Off"%self.hostname:
-            self.circle = False
-
 
 if __name__ == '__main__':
-#    if not (len(sys.argv) == 2):
-#        print "usage: controllerWaypoints.py <hostname>"
-#        sys.exit(-1)
-
-
- #   hostname = argv = sys.argv[1]
-
-        
     rospy.init_node('controller_robots')
     controller_waypoints = ControllerRobots()
-    #rospy.spin()
 
     r = rospy.Rate(20)
     while not rospy.is_shutdown():
