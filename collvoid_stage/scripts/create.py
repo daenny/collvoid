cd47cc41c83ebef666a008a719385fcf533412ed
danielclaes <devnull@localhost>
2012-10-18 11:40:20 +0200

    fuerte release preperation

--- collvoid_stage/scripts/create.py
+++ collvoid_stage/scripts/create.py
@@ -18,14 +18,15 @@ def create_world_file(argv):
     useNoise = False
     useBagFile = False
     bagFileName = "collvoid.bag"
+    useSticks = False
     try:
-        opts, args= getopt.getopt(argv, "hn:s:Rolxf:N", ["help","numRobots=","circleSize=","radiusScaling","omni","localization","experiments","bagFileName=","noise"])
+        opts, args= getopt.getopt(argv, "hn:s:olxf:S", ["help","numRobots=","circleSize=","omni","localization","experiments","bagFileName=","Sticks"])
     except getopt.GetoptError:
-        print 'create.py -n <numRobots> -s <circleSize> <-h> <-l> <-x> <-f> bagFile <-R>'
+        print 'create.py -n <numRobots> -s <circleSize> <-h> <-l> <-x> <-f> bagFile <-S>'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print 'create.py -n <numRobots> -s <circleSize> <-h> <-l> <-x><-f> bagfile <-R>'
+            print 'create.py -n <numRobots> -s <circleSize> <-h> <-l> <-x> <-f> bagfile <-S>'
             sys.exit(2)
         elif opt in ("-n","--numRobots"):
             numRobots = int(arg)
@@ -40,10 +41,9 @@ def create_world_file(argv):
         elif opt in ("-f", "--bagFileName"):
             useBagFile = True
             bagFileName = str(arg)
-        elif opt in ("-R", "--radiusScaling"):
-            scaleRadius = False
-        elif opt in ("-N"):
-            useNoise = True
+        elif opt in ("-S", "--Sticks"):
+            useSticks = True
+            omni = True
     
     direct = commands.getoutput('rospack find collvoid_stage')
     worldFileTemp = open(direct + '/world/swarmlab_template.world','r')
@@ -67,8 +67,11 @@ def create_world_file(argv):
         posX = circleSize*math.cos(angle/360*2*math.pi)
         
         posY = circleSize*math.sin(angle/360*2*math.pi)
-        if (omni):
-           worldFileNew.write('pr2( pose [ {0:f} {1:f} 0 {2:f} ] name "robot_{3:d}" color "{4}")\n'.format(centerX+posX,centerY+posY,anglePrint,x,cols[40 +  10 * x]))
+        if (omni or useSticks):
+            if(not useSticks):
+                worldFileNew.write('pr2( pose [ {0:f} {1:f} 0 {2:f} ] name "robot_{3:d}" color "{4}")\n'.format(centerX+posX,centerY+posY,anglePrint,x,cols[40 +  10 * x]))
+            else:
+                worldFileNew.write('stick( pose [ {0:f} {1:f} 0 {2:f} ] name "robot_{3:d}" color "{4}")\n'.format(centerX+posX,centerY+posY,anglePrint,x,cols[40 +  10 * x]))
         else:
             worldFileNew.write('roomba( pose [ {0:f} {1:f} 0 {2:f} ] name "robot_{3:d}" color "{4}")\n'.format(centerX+posX,centerY+posY,anglePrint,x,cols[40 +  10 * x]))
 
@@ -76,29 +79,14 @@ def create_world_file(argv):
     #tele_robot( pose [ -1 0 0 180.000 ] name "bob" color "blue")
     worldFileTemp.close()
     worldFileNew.close()
-    create_yaml_file(circleSize,numRobots,omni,simulation,localization,centerX,centerY,scaleRadius,useNoise)
-    create_launch_file(numRobots,omni,runExperiments,bagFileName,localization,useBagFile)
+    create_yaml_file(circleSize,numRobots,omni,simulation,localization,centerX,centerY,scaleRadius,useNoise, useSticks)
+    create_launch_file(numRobots,omni,runExperiments,bagFileName,localization,useBagFile, useSticks)
     
     
-def create_yaml_file(circleSize, numRobots,omni,simulation,localization,centerX,centerY,scaleRadius,useNoise):
+def create_yaml_file(circleSize, numRobots,omni,simulation,localization,centerX,centerY,scaleRadius,useNoise, useSticks):
     direct = commands.getoutput('rospack find collvoid_stage')
     yamlWrite = open(direct + '/params_created.yaml','w')
     yamlWrite.write("/use_sim_time: true\n")
-    yamlWrite.write("/simulation_mode: true\n")
-    if not localization:
-        yamlWrite.write('/use_ground_truth: true\n')
-    else:
-        yamlWrite.write('/use_ground_truth: false\n')
-    if (scaleRadius):
-       yamlWrite.write('/scale_radius: true\n')
-    else:
-       yamlWrite.write('/scale_radius: false\n')
-    if not useNoise:
-        yamlWrite.write("/init_guess_noise_std: 0.0\n")
-    else:
-        yamlWrite.write("/init_guess_noise_std: 0.20\n")
-  
-    yamlWrite.write('/num_robots: ' + str(numRobots) + '\n')
     
     angle = 360.0 / numRobots
     for x in range(numRobots):
@@ -114,51 +102,70 @@ def create_yaml_file(circleSize, numRobots,omni,simulation,localization,centerX,
     
     yamlWrite.close()
     
-def create_launch_file(numRobots,omni,runExperiments, bagFilename, localization,useBagFile):
+def create_launch_file(numRobots,omni,runExperiments, bagFilename, localization,useBagFile, useSticks):
     direct = commands.getoutput('rospack find collvoid_stage')
 
     launchWrite = open(direct + '/launch/sim_created.launch','w')
     launchWrite.write("<launch>\n")
     launchWrite.write('  <node name="map_server" pkg="map_server" type="map_server" args="$(find collvoid_stage)/world/swarmlab_map.yaml"/>\n')
     launchWrite.write('  <rosparam command="load" file="$(find collvoid_stage)/params_created.yaml"/>\n')
-   # if (runExperiments):
-   #     launchWrite.write('<node pkg="collvoid_controller" type="watchdog.py" name="watchdog" output="screen"/>\n')
-   # else:
-   #     launchWrite.write('<node pkg="collvoid_controller" type="controller.py" name="controller" output="screen"/>\n')
-    launchWrite.write('  <node pkg="stage" type="stageros" name="stageros" args="$(find collvoid_stage)/world/swarmlab_created.world" respawn="false" output="screen" />\n')
+    if runExperiments:
+        launchWrite.write('  <node pkg="stage" type="stageros" name="stageros" args="-g $(find collvoid_stage)/world/swarmlab_created.world" respawn="false" output="screen" />\n')
+    else:
+        launchWrite.write('  <node pkg="stage" type="stageros" name="stageros" args="$(find collvoid_stage)/world/swarmlab_created.world" respawn="false" output="screen" />\n')
+
     for x in range(numRobots):
-        if (omni):
-            launchWrite.write('  <include file="$(find collvoid_stage)/launch/amcl_omni_multi.launch">\n')
+        if (localization):
+            if (omni):
+                launchWrite.write('  <include file="$(find collvoid_stage)/launch/amcl_omni_multi.launch">\n')
+            else:
+                launchWrite.write('  <include file="$(find collvoid_stage)/launch/amcl_diff_multi.launch">\n')
+            launchWrite.write('    <arg name="robot" value="robot_{0}"/>\n'.format(x))
+            launchWrite.write('  </include>\n')
         else:
-            launchWrite.write('  <include file="$(find collvoid_stage)/launch/amcl_diff_multi.launch">\n')
-        launchWrite.write('    <arg name="robot" value="robot_{0}"/>\n'.format(x))
-        launchWrite.write('  </include>\n')
-        
-        launchWrite.write('  <node pkg="move_base" type="move_base" respawn="false" name="move_base" ns="robot_{0}" output="screen">\n'.format(x))
-        if (omni):
+            launchWrite.write('  <node name="fake_localization" pkg="fake_localization" ns="robot_{0}" type="fake_localization" respawn="false">\n'.format(x))
+            launchWrite.write('    <param name="~tf_prefix" value="robot_{0}" />\n'.format(x))
+            launchWrite.write('    <param name="~odom_frame_id" value="/robot_{0}/odom" />\n'.format(x))
+            launchWrite.write('    <param name="~base_frame_id" value="/robot_{0}/base_link" />\n'.format(x))
+            launchWrite.write('  </node>')
+
+
+            
+        launchWrite.write('  <node pkg="move_base" type="move_base" respawn="true" name="move_base" ns="robot_{0}" output="screen">\n'.format(x))
+        if (omni) and not useSticks:
             launchWrite.write('    <rosparam command="load" file="$(find collvoid_stage)/params/params_pr2.yaml" />\n')
+        elif useSticks:
+            launchWrite.write('    <rosparam command="load" file="$(find collvoid_stage)/params/params_stick.yaml" />\n')
         else:
             launchWrite.write('    <rosparam command="load" file="$(find collvoid_stage)/params/params_turtle.yaml" />\n')
+
+        if(runExperiments):
+            launchWrite.write('    <rosparam command="load" file="$(find collvoid_stage)/params/collvoid_exp_created.yaml" />\n')
+        else:
+            launchWrite.write('    <rosparam command="load" file="$(find collvoid_stage)/params/collvoid_config.yaml" />\n')
+
         launchWrite.write('    <remap from="map" to="/map" />\n')
         launchWrite.write('    <param name="~tf_prefix" value="robot_{0}" />\n'.format(x))
         launchWrite.write('    <param name="~/global_costmap/robot_base_frame" value="robot_{0}/base_link" /> \n    <param name="~/local_costmap/robot_base_frame" value="robot_{1}/base_link" /> \n    <param name="~/local_costmap/global_frame" value="robot_{0}/odom" /> \n'.format(x,x,x))
         launchWrite.write('    <param name="base_local_planner" value="collvoid_local_planner/CollvoidLocalPlanner" />\n')
         launchWrite.write('    <param name="base_global_planner" value="collvoid_simple_global_planner/CollvoidSimpleGlobalPlanner" />\n')
-        launchWrite.write('    <rosparam file="$(find pr2_navigation_config)/move_base/dwa_local_planner.yaml" command="load" ns="DWAPlannerROS" />\n')
+       # launchWrite.write('    <rosparam file="$(find pr2_navigation_config)/move_base/dwa_local_planner.yaml" command="load" ns="DWAPlannerROS" />\n')
        
         launchWrite.write('  </node> \n')
         launchWrite.write('  <node pkg="collvoid_controller" type="controllerRobots.py" name="controllerRobots" ns="robot_{0}" output="screen" />\n'.format(x))
-       
-    launchWrite.write('  <node pkg="collvoid_controller" type="controller.py" name="controller" output="screen" />\n')
-  
+    if (runExperiments):
+        launchWrite.write('  <node pkg="collvoid_controller" type="watchdog.py" name="watchdog" output="screen"/>\n')
+    else:
+        launchWrite.write('  <node pkg="collvoid_controller" type="controller.py" name="controller" output="screen"/>\n')
+        launchWrite.write('  <node pkg="rviz" type="rviz" name="rviz" args="-d $(find collvoid_stage)/double_view.vcg" output="screen" />\n')
+   
     s = ""
 
-#    for x in range(numRobots):
-#        s += "/robot_%d/debug "%(x)
+    for x in range(numRobots):
+        s += "/robot_%d/base_pose_ground_truth "%(x)
     if useBagFile:
-        launchWrite.write('  <node pkg="rosbag" type="record" name="rosbag" args="record {0} /stall /stall_resolved /exceeded -O $(find collvoid_stage)/{1}" output="screen"/>\n'.format(s,bagFilename))
+        launchWrite.write('  <node pkg="rosbag" type="record" name="rosbag" args="record {0} /position_share /stall /stall_resolved /num_run /exceeded -O $(find collvoid_stage)/bags/{1}" output="screen"/>\n'.format(s,bagFilename))
     
-    launchWrite.write('  <node pkg="rviz" type="rviz" name="rviz" args="-d $(find collvoid_stage)/double_view.vcg" output="screen" />\n')
     launchWrite.write("</launch>\n")
     launchWrite.close()
 
