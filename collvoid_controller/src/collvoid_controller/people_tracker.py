2c2b17c28589b334e114d66437786e07837d942a
Daniel Claes <daniel.claes@liverpool.ac.uk>
2015-06-25 18:07:54 +0100

    move launch files, track people

---
+++ collvoid_controller/src/collvoid_controller/people_tracker.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+from geometry_msgs.msg import PoseStamped, PolygonStamped, Point32
+
+__author__ = 'danielclaes'
+import rospy
+import tf
+from spencer_tracking_msgs.msg import TrackedPersons, TrackedPerson
+from collvoid_msgs.msg import AggregatedPoseTwist, PoseTwistWithCovariance
+from math import sin, cos, pi
+
+global_frame = "/map"
+radius = 0.5
+
+class PeopleTracker(object):
+    def __init__(self):
+        self.detected = []
+        self.people_pub = rospy.Publisher('people', AggregatedPoseTwist)
+        self.tf_listener = tf.Transformer()
+        self.footprint = PolygonStamped()
+        num_points = 32
+        for i in xrange(num_points):
+            p = Point32()
+            p.x = radius * cos((2. * pi * i) / num_points)
+            p.y = radius * sin((2. * pi * i) / num_points)
+            self.footprint.polygon.points.append(p)
+        rospy.sleep(0.5)
+        rospy.Subscriber('tracked_persons', TrackedPersons, self.detected_persons)
+
+    def detected_persons(self, msg):
+        """
+        :param msg:
+        :type msg: TrackedPersons
+        :return:
+        """
+        publish_msg = AggregatedPoseTwist()
+        publish_msg.header.stamp = msg.header.stamp
+        publish_msg.header.frame_id = global_frame
+        for track in msg.tracks:
+            person = PoseTwistWithCovariance()
+            person.header.stamp = msg.header.stamp
+            person.header.frame_id = global_frame
+            person.controlled = False
+            person.twist = track.twist
+            person.holo_robot = True
+            person.robot_id = "person_" + str(track.track_id)
+            person.footprint = self.footprint
+            pose = PoseStamped()
+            pose.header = msg.header
+            pose.pose = track.pose.pose
+            pose = self.transform_pose_to_global(pose)
+            person.pose.pose = pose.pose
+            person.pose.covariance = track.pose.covariance
+            if pose is None:
+                continue
+            publish_msg.posetwists.append(person)
+        self.people_pub.publish(publish_msg)
+
+    def transform_pose_to_global(self, pose):
+        try:
+            result = self.tf_listener.transformPose(global_frame, pose)
+        except tf.Exception as e:
+            rospy.logwarn("Peopletracker: could not transform pose, %s", e)
+            return None
+        return result
+
+
+if __name__ == '__main__':
+    rospy.init_node('people_tracker')
+    tracker = PeopleTracker()
+    rospy.spin()
+
